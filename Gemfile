67ffa12d0e9fdafd0de2de85e7b5843e4865917c
sorah (Shota Fukumori) <her@sorah.jp>
2016-04-03 06:33:09 +0900

    index.html

--- Gemfile
+++ Gemfile
@@ -5,3 +5,4 @@ source "https://rubygems.org"
 gem 'nokogiri'
 gem 'fluent-logger'
 gem 'aws-sdk'
+gem 'rack'
--- s3_cleanup.rb
+++ s3_cleanup.rb
@@ -8,6 +8,7 @@ require 'time'
 require 'uri'
 require 'open-uri'
 require 'nokogiri'
+require 'rack/utils'
 require 'fluent-logger'
 require 'aws-sdk'
 require 'json'
@@ -52,31 +53,80 @@ module Aandg
     end
 
     def run!
+      updated_program_prefixes = []
+
       existing_work_prefixes.each do |work_prefix|
-        run_on_work_prefix work_prefix
+        ran = run_on_work_prefix(work_prefix)
+        if ran
+          updated_program_prefixes << work_prefix.program_prefix
+        end
       end
+
+      updated_program_prefixes.each do |program_prefix|
+        program_prefix.recordings(:reload)
+        update_index program_prefix
+        update_feed program_prefix
+      end
+    end
+
+    def update_index(program_prefix)
+      key = "#{program_prefix.prefix}index.html"
+      existing_index = begin
+        s3.get_object(bucket: s3_bucket, key: key).body.read
+      rescue Aws::S3::Errors::NoSuchKey
+        "<ul>\n"
+      end
+      new_index = existing_index.dup
+      existing_index_html = Nokogiri::HTML(existing_index)
+
+      missing_recordings = program_prefix.recordings.reject do |x|
+        existing_index_html.at("li[data-agqr-pubdate-str='#{x.pubdate_str}']")
+      end
+
+      missing_recordings.sort_by!(&:pubdate)
+
+      missing_recordings.each do |x|
+        new_index << "<li data-agqr-pubdate-str='#{Rack::Utils.escape_html(x.pubdate_str)}' data-agqr-meta=\"#{Rack::Utils.escape_html(x.metadata.to_json)}\">"
+        new_index << "<a href='/#{x.meta.key}'>#{x.pubdate_str}</a>: "
+        if x.metadata['program']
+          new_index << "#{Rack::Utils.escape_html(x.metadata['program']['title'])} - #{x.metadata['program']['personality']}"
+        end
+        new_index << " <a href='/#{x.mp3.key}'>mp3</a>"
+        new_index << " <a href='/#{x.mp4.key}'>mp4</a>"
+        new_index << "</li>\n"
+      end
+
+      s3.put_object(
+        bucket: s3_bucket,
+        key: key,
+        body: new_index,
+        content_type: 'text/html',
+      )
+    end
+
+    def update_feed(program_prefix)
     end
 
     def run_on_work_prefix(work_prefix)
       puts "=> #{work_prefix.prefix}"
       unless work_prefix.target?
         puts " * Not a target, skip"
-        return
+        return false
       end
 
       if work_prefix.host_works.empty?
         puts " * WARN: no host works, skip"
-        return
+        return false
       end
 
       worker = work_prefix.worker
       if worker && worker != hostname
         puts " * #{worker.inspect} is working on, skipping"
-        return
+        return false
       elsif !worker
         unless work_prefix.won_vote?
           puts " * #{work_prefix.vote_winner.host.inspect} won a vote, skipping"
-          return
+          return false
         end
       end
 
@@ -92,19 +142,21 @@ module Aandg
           puts " * WARN: meta json already exists! skipping"
           return false
         end
-      rescue Aws::S3::Errors::NoSuchKey
+      rescue Aws::S3::Errors::NoSuchKey, Aws::S3::Errors::NotFound
       end
 
       best_work = work_prefix.best_work
       puts " * best work is #{best_work.prefix.inspect}"
 
       puts " * Extracting best mp3 & mp4 to #{program_prefix.prefix.inspect}"
-      best_work.extract_works(program_prefix.prefix)
+      best_work.extract_works("#{program_prefix.prefix}rec/")
+
+      rec_prefix = "#{program_prefix.prefix}rec/#{work_prefix.pubdate_str}/"
 
       if best_work.error_count > 0
         puts " * best work's error count is #{best_work.error_count}, keeping other host's work"
         work_prefix.host_works.each do |host_work|
-          new_prefix = "#{program_prefix.prefix}#{work_prefix.pubdate_str}/#{host_work.host}/"
+          new_prefix = "#{rec_prefix}#{host_work.host}/"
           puts " - #{host_work.prefix.inspect} => #{new_prefix.inspect}"
           host_work.move_to!(new_prefix)
         end
@@ -112,13 +164,15 @@ module Aandg
         puts " * removing host works"
         work_prefix.host_works.each do |x|
           if x == best_work
-            new_prefix = "#{program_prefix.prefix}#{work_prefix.pubdate_str}/#{x.host}/"
+            new_prefix = "#{rec_prefix}#{x.host}/"
             x.move_to!(new_prefix)
           else
             x.destroy!
           end
         end
       end
+
+      true
     ensure
       work_prefix.declare_work_finish!
     end
@@ -137,6 +191,49 @@ module Aandg
         @work_prefixes ||= @s3.list_objects(bucket: bucket, prefix: "#{prefix}work/", delimiter: '/').flat_map(&:common_prefixes).map do |x|
           WorkPrefix.new(@s3, bucket, x.prefix, @hostname, self)
         end
+      end
+
+      def recordings(reload = false)
+        @recordings = nil if reload
+        @recordings ||= begin
+          rec_prefix = "#{prefix}rec/"
+          @s3.list_objects(bucket: bucket, prefix: rec_prefix, delimiter: '/').flat_map(&:contents).group_by do |content|
+            content.key[rec_prefix.size..-1].split(?/,2).first.sub(%r{\..+\z},'')
+          end.map do |pubdate_str, contents|
+            Recording.new(@s3, bucket, pubdate_str, contents)
+          end
+        end
+      end
+    end
+
+    class Recording
+      def initialize(s3, bucket, pubdate_str, contents)
+        @s3 = s3
+        @bucket = bucket
+        @pubdate_str = pubdate_str
+        @contents = contents
+      end
+
+      attr_reader :bucket, :pubdate_str, :contents
+
+      def pubdate
+        @pubdate ||= Time.strptime(pubdate_str, '%Y-%m-%d_%H%M%S')
+      end
+
+      def mp4
+        @mp4 ||= contents.find { |_| _.key.end_with? "rec/#{pubdate_str}.mp4" }
+      end
+
+      def mp3
+        @mp3 ||= contents.find { |_| _.key.end_with? "rec/#{pubdate_str}.mp3" }
+      end
+
+      def meta
+        @meta ||= contents.find { |_| _.key.end_with? "rec/#{pubdate_str}.json" }
+      end
+
+      def metadata
+        @metadata ||= meta && JSON.parse(@s3.get_object(bucket: bucket, key: meta.key).body.read)
       end
     end
 
